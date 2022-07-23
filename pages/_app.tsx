75ff2ab410a02f239076a25f913fc81f277ad632
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-07-23 18:35:36 -0400

    amplify wrapped & misc
    user credentials showing on homepage - aws amplfy working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import awsExports from "../src/aws-exports";
+import Amplify from "aws-amplify";
+
+Amplify.configure(awsExports);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
